a4d5ea50b17d426ed766caff0ada45e6ce9d12f0
zhangyong [张勇] <zhangyong@xiaomi.com>
2018-06-24 21:18:47 +0800

    parameters

--- titanic/model/decision_tree.py
+++ titanic/model/decision_tree.py
@@ -1,6 +1,7 @@
-from sklearn import tree
+from sklearn import tree, svm
 from sklearn.ensemble import GradientBoostingClassifier
 import pandas as pd 
+from sklearn.model_selection import train_test_split, GridSearchCV
 
 def preprocess(train_path, test_path):
     train = pd.read_csv(train_path)
@@ -16,11 +17,19 @@ def preprocess(train_path, test_path):
 def train_model(dat):
     y = dat['Survived']
     X = dat.drop(['Survived'], axis=1)
-    X_train, X_test = X[:800], X[800:]
-    y_train, y_test = y[:800], y[800:]
-    clf = GradientBoostingClassifier(n_estimators=100, 
-    learning_rate=0.1, max_depth=10, random_state=0).fit(X_train, y_train)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+    params = [
+        {'n_estimators':[100], 'learning_rate':[1.0], 'max_depth': [10], 'random_state':[0]},
+        {'n_estimators':[120], 'learning_rate': [0.1], 'max_depth': [30], 'random_state':[0]},
+        {'n_estimators':[60], 'learning_rate': [0.5], 'max_depth': [50], 'random_state':[0]}
+    ]
+    scores = ['precision', 'recall']
+    clf = GridSearchCV(GradientBoostingClassifier(), params, cv=5, scoring='accuracy')
+    # clf = GradientBoostingClassifier(n_estimators=100, 
+    # learning_rate=0.2, max_depth=10, random_state=0).fit(X_train, y_train)
+    clf.fit(X, y)
     print(clf.score(X_test, y_test))
+    print(clf.best_params_)
     return clf
 
 def output_predict(model, test, out_path):
